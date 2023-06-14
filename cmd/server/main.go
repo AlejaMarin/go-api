92116ed3765132d0a167d20101bd188c6ccb1c10
AlejaMarin <maialemarin@gmail.com>
2023-06-14 17:51:58 -0500

    Ejercicio en Clase

---
+++ cmd/server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/AlejaMarin/go-api/cmd/server/handler"
+	"github.com/AlejaMarin/go-api/internal/domain"
+	"github.com/AlejaMarin/go-api/internal/product"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var productsList = []domain.Product{}
+	loadProducts("../../products.json", &productsList)
+
+	repo := product.NewRepository(productsList)
+	service := product.NewService(repo)
+	productHandler := handler.NewProductHandler(service)
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
+	products := r.Group("/products")
+	{
+		products.GET("", productHandler.GetAll())
+		products.GET(":id", productHandler.GetByID())
+		products.GET("/search", productHandler.Search())
+		products.POST("", productHandler.Post())
+		products.PUT(":id", productHandler.Put())
+	}
+	r.Run(":8080")
+}
+
+// loadProducts carga los productos desde un archivo json
+func loadProducts(path string, list *[]domain.Product) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(file), &list)
+	if err != nil {
+		panic(err)
+	}
+}
